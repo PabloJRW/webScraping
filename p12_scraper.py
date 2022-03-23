8d591e2afc42c7b3b4438fb9d9eb34e2070d36ac
PabloJRW <ramoswilkinspj28@gmail.com>
2022-03-22 20:59:25 -0500

    Terminé las funciones para el raspado completo

--- p12_scraper.py
+++ p12_scraper.py
@@ -1,69 +1,82 @@
 import requests
 from bs4 import BeautifulSoup
-import os
-import datetime
+import re
 
-HOME_URL = 'https://www.pagina12.com.ar/'
-
-XPATH_LINK_TO_ARTICLE = '//div[@class="story-box"]/h2/a/@href'
-XPATH_TITLE = '//article/div[@class="article-header"]/h1/text()'
-XPATH_SUMMARY = '//article/div[@class="article-header"]/div[@class="article-summary"]/text()'
-XPATH_BODY = '//article/div[@class="article-content"]/p[not(@class)]/text()'
-
-def parse_news(link, today):
+def getHomePage(HOME_URL):
+    """Recibe el enlace de la página y hace la consulta"""
+    
     try:
-        response = requests.get(link)
+        response = requests.get(HOME_URL) # Consulta a la página a scrapear
         if response.status_code == 200:
-            parsed = html.fromstring(response)
-
-            try:
-                title = parsed.xpath(XPATH_TITLE)[0]
-                title = title.replace('\"', '')
-                summary = parsed.xpath(XPATH_SUMMARY)[0]
-                body = parsed.xpath(XPATH_BODY)
-            except IndexError:
-                return
-
-            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
-                f.write(title)
-                f.write('\n\n')
-                f.write(summary)
-                f.write('\n\n')
-                for p in body:
-                    f.write(p)
-                    f.write('\n')
-
-        else:
-            raise ValueError(f'Error: {response.status_code}')
-    except ValueError as ve:
-        print(ve)
-
-
-def parse_home():
-    try:
-        response = requests.get(HOME_URL)
-        if response.status_code == 200:
-            # Home Page
             home_soup = BeautifulSoup(response.content, 'lxml')
-
-            # Sections Navigator
-            nav = home_soup.find('ul', attrs={'class':'horizontal-list main-sections hide-on-dropdown'}).find_all('li')
-            nav_sections = [sect.a.get('href') for sect in nav_sections]
-
-            
-
-        else:
-            raise ValueError(f'Error: {response.status_code}')
-    except ValueError as ve:
-        print(ve)
+            return home_soup
         
+    except Exception as e:
+        print(e)
 
 
-def run():
-    parse_home()
+def getNavSections(home_soup):
+    """Recibe la sopa de la página principal, retorna los enlaces de las secciones"""
+    
+    nav_sect_soup = home_soup.find('ul', attrs={'class':'horizontal-list main-sections hide-on-dropdown'}).find_all('li')
+    nav_sections = [section.a.get('href') for section in nav_sect_soup]
+    return nav_sections
 
 
-if __name__=='__main__':
-    run()
+def getSectionNewsLinks(sections):
+    """Recibe los enlaces de las secciones, retorna los enlaces de las noticias en ellas """
+    
+    links_to_news = []
+    for section in sections:
+        section_response = requests.get(section) # Consulta las secciones
+        
+        try:
+            if section_response.status_code == 200:
+                section_soup = BeautifulSoup(section_response.content, 'lxml')
+                articles = section_soup.find('div', attrs={'id':'main-content'}).find_all('article')
+                news_links = [i.a.get('href') for i in articles]
+                links_to_news.append(news_links)
+                
+        except Exception as e:
+            print(e)   
+            
+    return links_to_news
 
 
+def getInfo(HOME_URL, sections_news_links):
+    
+    info = []
+    for list_section_links in sections_news_links:
+        for article in list_section_links:
+        
+            news_response = requests.get(HOME_URL + article)
+            
+            try:
+                if news_response.status_code == 200:
+                    article_soup = BeautifulSoup(news_response.content, 'lxml')
+                    
+                    # Título de la noticia
+                    title = article_soup.find('title').get_text() 
+                    title = re.sub(r'Página12', '', title)
+                    
+                    # Fecha de publicación
+                    date = article_soup.find('span').get_text()
+                    
+                    #Resumen de la noticia
+                    summary = article_soup.find('div',attrs={'class':'col 2-col'}).find('h3').get_text()
+                    
+                    # Cuerpo de la noticia
+                    body = article_soup.find('div', attrs={'class':'article-main-content article-text'}).find_all('p')
+                    body = [p.get_text() for p in body]
+                    
+                    info.append({'title': title, 
+                                 'date': date
+                                 'summary': summary,
+                                 'body' : body})
+                    
+                    print(body)
+                    
+            except Exception as e:
+                print(e)
+                    
+    return info
